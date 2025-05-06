b5051218c0028cbb75d1366cf502f405613b7858
ehsemloh <ehsemloh@gmail.com>
2025-05-06 20:02:34 +1200

    chore: initial commit

---
+++ ios/Agent.h
@@ -0,0 +1,5 @@
+#import <AgentSpec/AgentSpec.h>
+
+@interface Agent : NSObject <NativeAgentSpec>
+
+@end
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import Agent from './NativeAgent';
+
+export function multiply(a: number, b: number): number {
+  return Agent.multiply(a, b);
+}
